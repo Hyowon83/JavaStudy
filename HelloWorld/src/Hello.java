75ebb0c0667fa770f62778614c414be6ab54fd68
Hyowon83 <aile1383@gmail.com>
2021-07-21 17:54:45 +0900

    210721수 데이터타입 변수

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,11 +1,7 @@
 
 public class Hello {
-
 	public static void main(String[] args) {
-		System.out.println("ÀåÈ¿¿ø");
-		System.out.println("010-9950-0240");
-		System.out.println("02.12");
-
+		String name = "Hyowon";
+		System.out.println(name);
 	}
-
 }
