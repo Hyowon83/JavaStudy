ae42ee00b9b50480107489cef258894eb37fcf9c
Hyowon83 <aile1383@gmail.com>
2021-07-21 16:08:26 +0900

    210721수 Hello World

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("ÀåÈ¿¿ø");
+		System.out.println("010-9950-0240");
+		System.out.println("02.12");
+
+	}
+
+}
