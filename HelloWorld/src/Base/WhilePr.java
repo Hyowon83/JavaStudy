64995243684a2b127fd9193285a5cdd7a01f4fb5
Hyowon83 <aile1383@gmail.com>
2021-07-23 17:45:28 +0900

    210723금 while 정리

---
+++ HelloWorld/src/Base/WhilePr.java
@@ -0,0 +1,40 @@
+package Base;
+
+public class WhilePr {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		while(a<=100) {
+			if(a%5==0) {
+				System.out.println(a);
+			}
+			a++;
+		}
+
+//		int o = 1;
+//		while(o<=100) {
+//			if(o%2==1) {
+//				System.out.println(o);
+//			}
+//			o++;
+//		}
+//		
+//		System.out.println();
+//		
+//		int n = 1;
+//		while(n<=12) {
+//			System.out.println(n);
+//			n++;
+//		}
+//		
+//		System.out.println();
+//		
+//		int i = 1;
+//		while(i<=100) {
+//			System.out.println(i);
+//			i++;
+//		}
+
+	}
+}
