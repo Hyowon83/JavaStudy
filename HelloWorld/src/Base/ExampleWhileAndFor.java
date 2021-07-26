01ba9a896b6813e0359271bcf45eade0e27786ae
Hyowon83 <aile1383@gmail.com>
2021-07-26 11:51:17 +0900

    210726월 while과 for문을 활용한 구구단 출력하기

--- HelloWorld/src/Base/ExampleWhileAndFor.java
+++ HelloWorld/src/Base/ExampleWhileAndFor.java
@@ -3,16 +3,16 @@ package Base;
 public class ExampleWhileAndFor {
 
 	public static void main(String[] args) {
-		int a = 2;
-		while(a<=9) {
-			int b = 1;
-			while(b<=9) {
-				System.out.printf("%d x %d = %2d\n", a, b, a*b);
-				b++;
-			}
-			System.out.println();
-			a++;
-		}
+//		int a = 2;
+//		while(a<=9) {
+//			int b = 1;
+//			while(b<=9) {
+//				System.out.printf("%d x %d = %2d\n", a, b, a*b);
+//				b++;
+//			}
+//			System.out.println();
+//			a++;
+//		}
 		
 		for(int n=2;n<=9;n++) {
 			for(int m=1;m<=9;m++) {
