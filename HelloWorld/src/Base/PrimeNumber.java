5da809f7414f2359aa428c5236f256d949ac0105
Hyowon83 <aile1383@gmail.com>
2021-07-26 19:41:27 +0900

    210726월 소수구하기(외워야함)

--- HelloWorld/src/Base/PrimeNumber.java
+++ HelloWorld/src/Base/PrimeNumber.java
@@ -8,11 +8,16 @@ public class PrimeNumber {
 //		final Scanner sc = new Scanner(System.in);
 //		int n = sc.nextInt();
 		
-		for(int n = 2; n <= 10; n++) {
-			for(int m = n; m <= n-1; m++) {
+
+		for(int n = 2; n <= 100; n++) {
+			int flag = 1;
+			for(int m = 2; m <= n-1; m++) {
 				if(n%m==0) {
+					flag = 0;
 					break;
 				}
+			}
+			if(flag == 1) {				
 				System.out.println(n);
 			}
 		}
