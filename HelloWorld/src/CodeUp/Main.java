862a5007c75112abfbe1c37012a3bf47a5e0ab7d
Hyowon83 <aile1383@gmail.com>
2021-07-27 17:42:58 +0900

    210727화 코드업 100제

--- HelloWorld/src/CodeUp/Main.java
+++ HelloWorld/src/CodeUp/Main.java
@@ -6,9 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		System.out.printf("%d:%d", a, b);
+		float b = sc.nextFloat();
+		System.out.printf("%d.%d", b);
 	}
 
 }
