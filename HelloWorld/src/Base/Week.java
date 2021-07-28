9a8cb97e727802d967138f6ff9b27d46b650c0ac
Hyowon83 <aile1383@gmail.com>
2021-07-28 15:20:09 +0900

    210728수 enum으로 사용자 정의 리스트 만들기

---
+++ HelloWorld/src/Base/Week.java
@@ -0,0 +1,11 @@
+package Base;
+//사용자 정의 타입(User-Defined Type)
+public enum Week { //enum은 같은 프로젝트에서 쓸 수 있는 리스트를 생성한다.
+	MONDAY,
+	TUESDAY,
+	WEDNESEDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
