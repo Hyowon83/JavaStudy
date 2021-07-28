942e13771f4afe31bfbfa04850866bdaf3d9a305
Hyowon83 <aile1383@gmail.com>
2021-07-28 15:20:23 +0900

    210728수 enum 사용하기

---
+++ HelloWorld/src/Base/EnumPr.java
@@ -0,0 +1,12 @@
+package Base;
+
+public class EnumPr {
+
+	public static void main(String[] args) {
+		// enum파일로 만든 Week리스트를 가져다가 쓸 수 있다.
+		Week weekdays;
+		weekdays = Week.WEDNESEDAY;
+		System.out.println(weekdays);
+	}
+
+}
