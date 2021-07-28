ea07ab93598c2eafa7cc97e115f1d9ba36b9f5cb
Hyowon83 <aile1383@gmail.com>
2021-07-28 21:53:51 +0900

    210728수 Car클래스로 인스턴스변수 만들기

---
+++ HelloWorld/src/Vehicle/Car.java
@@ -0,0 +1,22 @@
+package Vehicle;
+
+import java.util.ArrayList;
+
+public class Car {
+	// 속성 = 변수 =  필드(자바) = 멤버변수(C++)
+	//필드는 초기값을 굳이 정해주지 않는게 좋다.
+	String color;
+	int MaxSpeed;
+	int nSpeed;
+	int guest;
+	String ModelName;
+	int nWheel;
+	ArrayList<Integer> DailyRun;
+	//int nFuel;
+	//int nYear;
+	//int nPrice;
+	//String FuelType; //gasoline/electricity/LPG/Hybrid...
+	//ArrayList<String> alName;
+	// 동작 = 함수(메소드) = 메소드(자바) = 멤버함수(C++)
+	
+}
