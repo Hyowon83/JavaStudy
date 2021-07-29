9eb6c179e7ae621c9d7d569a0e29a51801b20103
Hyowon83 <aile1383@gmail.com>
2021-07-29 17:31:22 +0900

    210729목 인스턴스변수에서 함수 사용하기.(프라이빗)

--- HelloWorld/src/Vehicle/AutoMobile.java
+++ HelloWorld/src/Vehicle/AutoMobile.java
@@ -1,6 +1,6 @@
 package Vehicle;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class AutoMobile {
 
@@ -10,31 +10,42 @@ public class AutoMobile {
 		Car seltos = new Car(); // 같은 프로젝트 안에 있어야 가져다가 쓸 수 있음.
 		
 		//필드의 값을 setting
-		seltos.color = "White";
-		seltos.MaxSpeed = 240;
-		seltos.nSpeed = 0;
-		seltos.guest = 5;
-		seltos.ModelName = "Trendy";
-		seltos.nWheel = 4;
-		seltos.DailyRun = new ArrayList<Integer>();
-		seltos.DailyRun.add(30);
-		seltos.DailyRun.add(43);
-		seltos.DailyRun.add(56);
-		seltos.DailyRun.add(77);
+		// seltos.color = "White";
+//		seltos.MaxSpeed = 240;
+//		seltos.nSpeed = 0;
+//		seltos.guest = 5;
+//		seltos.ModelName = "Trendy";
+//		seltos.nWheel = 4;
+//		seltos.DailyRun = new ArrayList<Integer>();
+//		seltos.DailyRun.add(30);
+//		seltos.DailyRun.add(43);
+//		seltos.DailyRun.add(56);
+//		seltos.DailyRun.add(77);
+//		
+//		//필드의 값을 getting
+//		System.out.println("색상 : " + seltos.color);
+//		System.out.println("최고속도 : " + seltos.MaxSpeed);
+//		System.out.println("현재속도 : " + seltos.nSpeed);
+//		System.out.println("최대승객수 : " + seltos.guest);
+//		System.out.println("모델명 : " + seltos.ModelName);
+//		System.out.println("바퀴갯수 : " + seltos.nWheel);
+//		for(int run:seltos.DailyRun) { // advance for
+//			System.out.println(run);
+//		}
+//		for(int i = 0; i < seltos.DailyRun.size(); i++) { // legacy for
+//			System.out.println(seltos.DailyRun.get(i));
+//		}
+//		System.out.println();
 		
-		//필드의 값을 getting
-		System.out.println("색상 : " + seltos.color);
-		System.out.println("최고속도 : " + seltos.MaxSpeed);
-		System.out.println("현재속도 : " + seltos.nSpeed);
-		System.out.println("최대승객수 : " + seltos.guest);
-		System.out.println("모델명 : " + seltos.ModelName);
-		System.out.println("바퀴갯수 : " + seltos.nWheel);
-		for(int run:seltos.DailyRun) { // advance for
-			System.out.println(run);
-		}
-		for(int i = 0; i < seltos.DailyRun.size(); i++) { // legacy for
-			System.out.println(seltos.DailyRun.get(i));
-		}
+		//함수로 set, get 후 출력
+		seltos.setColor("Blue");
+		System.out.println("색상 : " + seltos.getColor());
+		seltos.setGuest(3);
+		System.out.println("승객수 : " + seltos.getGuest());
+		seltos.drive(50);
+		System.out.println("현재속도 : " + seltos.getnSpeed());
+		seltos.stop();
+		System.out.println("정지 후 속도 : " + seltos.getnSpeed());
 	}
 
 }
--- HelloWorld/src/Vehicle/Car.java
+++ HelloWorld/src/Vehicle/Car.java
@@ -5,18 +5,67 @@ import java.util.ArrayList;
 public class Car {
 	// 속성 = 변수 =  필드(자바) = 멤버변수(C++)
 	//필드는 초기값을 굳이 정해주지 않는게 좋다.
-	String color;
-	int MaxSpeed;
-	int nSpeed;
-	int guest;
-	String ModelName;
-	int nWheel;
-	ArrayList<Integer> DailyRun;
+	private String color;
+	private int MaxSpeed;
+	private int nSpeed;
+	private int guest;
+	private String ModelName;
+	private int nWheel;
+	private ArrayList<Integer> DailyRun;
 	//int nFuel;
 	//int nYear;
 	//int nPrice;
 	//String FuelType; //gasoline/electricity/LPG/Hybrid...
 	//ArrayList<String> alName;
+	
 	// 동작 = 함수(메소드) = 메소드(자바) = 멤버함수(C++)
+	void drive(int x) {
+		this.nSpeed = x;
+	}
+	void stop() {
+		this.nSpeed = 0;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getMaxSpeed() {
+		return MaxSpeed;
+	}
+	public void setMaxSpeed(int maxSpeed) {
+		MaxSpeed = maxSpeed;
+	}
+	public int getnSpeed() {
+		return nSpeed;
+	}
+	public void setnSpeed(int nSpeed) {
+		this.nSpeed = nSpeed;
+	}
+	public int getGuest() {
+		return guest;
+	}
+	public void setGuest(int guest) {
+		this.guest = guest;
+	}
+	public String getModelName() {
+		return ModelName;
+	}
+	public void setModelName(String modelName) {
+		ModelName = modelName;
+	}
+	public int getnWheel() {
+		return nWheel;
+	}
+	public void setnWheel(int nWheel) {
+		this.nWheel = nWheel;
+	}
+	public ArrayList<Integer> getDailyRun() {
+		return DailyRun;
+	}
+	public void setDailyRun(ArrayList<Integer> dailyRun) {
+		DailyRun = dailyRun;
+	}
 	
 }
