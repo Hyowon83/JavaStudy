0766210683627f4d37fd7dff71cf1543fad6e479
Hyowon83 <aile1383@gmail.com>
2021-07-30 17:34:33 +0900

    210730금 public을 사용해서 외부에서 사용하기

--- HelloWorld/src/Vehicle/Car.java
+++ HelloWorld/src/Vehicle/Car.java
@@ -18,11 +18,35 @@ public class Car {
 	//String FuelType; //gasoline/electricity/LPG/Hybrid...
 	//ArrayList<String> alName;
 	
+	//생성자 오버로딩(constructor overloading) <- 다형성
+	public Car() { //face //기본 생성자(default constructor)
+		//body
+//		this.color = "Blue";
+//		this.guest = 5;
+//		this.MaxSpeed = 240;
+//		this.nSpeed = 0;
+		this("Blue", 5, 240, 0); //생성자가 생성자를 호출 할 수 있다.(매개변수 갯수가 맞는애로 알아서 찾아감)
+	}
+	public Car(String a, int b, int c, int d) {
+		this.color = a;
+		this.guest = b;
+		this.MaxSpeed = c;
+		this.nSpeed = d;
+	}
+	public Car(String n, int m, int x, int y, String z, int w) {
+		this.color = n;
+		this.guest = m;
+		this.MaxSpeed = x;
+		this.nSpeed = y;
+		this.ModelName = z;
+		this.nWheel = w;
+	}
+	
 	// 동작 = 함수(메소드) = 메소드(자바) = 멤버함수(C++)
-	void drive(int x) {
+	public void drive(int x) {
 		this.nSpeed = x;
 	}
-	void stop() {
+	public void stop() {
 		this.nSpeed = 0;
 	}
 	public String getColor() {
