9c41750c9072b6eb8817cd5c67bf58b96a3ac52c
Hyowon83 <aile1383@gmail.com>
2021-07-30 17:34:00 +0900

    210730금 생성자와 매개변수

--- HelloWorld/src/Vehicle/AutoMobile.java
+++ HelloWorld/src/Vehicle/AutoMobile.java
@@ -5,9 +5,31 @@ package Vehicle;
 public class AutoMobile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//클래스 호출.
 		Car seltos = new Car(); // 같은 프로젝트 안에 있어야 가져다가 쓸 수 있음.
+		//바로 기본 생성자가 실행되고 호출됨.
+		System.out.println(seltos.getColor());
+		System.out.println(seltos.getGuest());
+		System.out.println(seltos.getMaxSpeed());
+		System.out.println(seltos.getnSpeed());
+		System.out.println();
+		
+		Car avante = new Car("White",5,280,50); //매개를 갖고있는 생성자가 호출됨.
+		System.out.println(avante.getColor());
+		System.out.println(avante.getGuest());
+		System.out.println(avante.getMaxSpeed());
+		System.out.println(avante.getnSpeed());
+		System.out.println();
+		
+		Car grandeur = new Car("Black", 5, 320, 50, "Granadeur", 4);
+		System.out.println(grandeur.getColor());
+		System.out.println(grandeur.getGuest());
+		System.out.println(grandeur.getMaxSpeed());
+		System.out.println(grandeur.getnSpeed());
+		System.out.println(grandeur.getModelName());
+		System.out.println(grandeur.getnWheel());
+		System.out.println();
+		
 		
 		//필드의 값을 setting
 		// seltos.color = "White";
@@ -38,14 +60,14 @@ public class AutoMobile {
 //		System.out.println();
 		
 		//함수로 set, get 후 출력
-		seltos.setColor("Blue");
-		System.out.println("색상 : " + seltos.getColor());
-		seltos.setGuest(3);
-		System.out.println("승객수 : " + seltos.getGuest());
-		seltos.drive(50);
-		System.out.println("현재속도 : " + seltos.getnSpeed());
-		seltos.stop();
-		System.out.println("정지 후 속도 : " + seltos.getnSpeed());
+//		seltos.setColor("Blue");
+//		System.out.println("색상 : " + seltos.getColor());
+//		seltos.setGuest(3);
+//		System.out.println("승객수 : " + seltos.getGuest());
+//		seltos.drive(50);
+//		System.out.println("현재속도 : " + seltos.getnSpeed());
+//		seltos.stop();
+//		System.out.println("정지 후 속도 : " + seltos.getnSpeed());
 	}
 
 }
