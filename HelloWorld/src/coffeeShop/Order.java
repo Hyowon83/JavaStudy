1cf7a0e7167034b158ef6e9028cfc872de71a935
Hyowon83 <aile1383@gmail.com>
2021-08-04 18:46:58 +0900

    210804수 대용량 메뉴(주문)

--- HelloWorld/src/coffeeShop/Order.java
+++ HelloWorld/src/coffeeShop/Order.java
@@ -2,10 +2,50 @@ package coffeeShop;
 import java.util.ArrayList;
 
 public class Order {
-	ArrayList<String> alMobile;
-	ArrayList<String> alMenu;
-	ArrayList<Integer> alCount;
-	ArrayList<Integer> alTotal;
+	Menu menu = new Menu();
+	private ArrayList<String> alMobile;
+	private ArrayList<String> alMenu;
+	private ArrayList<Integer> alCount;
+	private ArrayList<Integer> alTotal;
 	
+	Order() {
+		alMobile = new ArrayList<String>();
+		alMenu = new ArrayList<String>();
+		alCount = new ArrayList<Integer>();
+		alTotal = new ArrayList<Integer>();
+	}
+	
+	int addOrder(String name, int price, int cnt) {
+		//this.alMobile.add(mobile);
+		this.alMenu.add(name);
+		this.alCount.add(cnt);
+		
+		int total = price * cnt;
+		this.alTotal.add(total);
+
+		System.out.printf("주문메뉴 : %s\n메뉴가격 : %d 원\n주문수량 : %d 개\n이 메뉴의 총액 : %d 원\n", name, price, cnt, total);
+		System.out.println();
+		
+		return total;
+	}
+	int getTotalSum() {
+		//주문리스트의 총액을 모두 더해서 합계를 반환
+		int nTotal = 0;
+		for(int i : this.alTotal) {
+			nTotal += i;
+		}
+		return nTotal;
+	}
+	String addMobile(String mobile) {
+		this.alMobile.add(mobile);
+		return mobile;
+	}
+	
+	void totalMenu() {
+		System.out.println("<주문내역>");
+		for(int i = 0; i < this.alMenu.size(); i++) {
+			System.out.printf("%d %s \t %d 잔\t%d 원\n", (i+1), this.alMenu.get(i) ,this.alCount.get(i), this.alTotal.get(i));
+		}
+	}
 
 }
