3e6b888372ba21ee41c7c7807b4081ba8091539d
Hyowon83 <aile1383@gmail.com>
2021-08-04 18:46:31 +0900

    210804수 백준 코딩테스트 연습장

--- HelloWorld/src/CodeUp/Main.java
+++ HelloWorld/src/CodeUp/Main.java
@@ -5,9 +5,28 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		float b = sc.nextFloat();
-		System.out.printf("%d.%d", b);
+		 Scanner sc=new Scanner(System.in);
+	     String money = sc.nextLine();
+	     int a = Integer.parseInt(money.split(" ")[0]);
+	     int b = Integer.parseInt(money.split(" ")[1]);
+	     int c = Integer.parseInt(money.split(" ")[2]);
+	     
+	     if(c <= b) {
+	    	 System.out.println(-1);
+	     } else {	    	 
+	    	 System.out.println((a / (c-b)) + 1);
+	     }
+	     
+//	     if(b < c) {
+//	    	 
+//	    	 int i = 1;
+//	    	 for(i = 1; (c * i) <= a + (b * i); i++);
+//	    	 System.out.println(i);
+//	    	 
+//	     } else {
+//	    	 System.out.println(-1);
+//	     }
+	     
+	     sc.close();
 	}
-
 }
