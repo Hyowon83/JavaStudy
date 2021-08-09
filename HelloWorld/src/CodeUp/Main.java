d42db520f4078ccfc23a71744128e084093f09dd
Hyowon83 <aile1383@gmail.com>
2021-08-09 09:20:43 +0900

    210809월 백준 별찍기5

--- HelloWorld/src/CodeUp/Main.java
+++ HelloWorld/src/CodeUp/Main.java
@@ -10,30 +10,18 @@ public class Main {
 		
 		int n = sc.nextInt();
 		
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= n; j++) {
-				if(j >= i) {					
-					System.out.print("*");
-				} else {
-					System.out.print(" ");
-				}
+		for(int i = 0; i < n; i++) {
+			for(int j = 1; j < n - i; j++){
+				System.out.print(" "); 
+			}
+			for(int j = 0; j < 2 * i + 1; j++){
+				System.out.print("*"); 
 			}
 			System.out.println();
 		}
 		
 		sc.close();
-		
-		for(int i=0;i<4;i++){
-			for(int j=0;j<3-i;j++){
-				System.out.print(" "); 
-			}
-			for(int j=0;j<2*i+1;j++){
-				System.out.print("*"); 
-			}
-			System.out.println("");
-		}
-		
-		
+
 //		// 백의자리 십의자리 일의자리 구하기
 //		int a = 653;
 //		System.out.printf("백의자리 : %d\n",a/100);
