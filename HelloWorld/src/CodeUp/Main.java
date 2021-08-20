95c9f5890a4361bee6f614bed94fb4828ad74943
Hyowon83 <aile1383@gmail.com>
2021-08-20 10:52:34 +0900

    별찍기 13

--- HelloWorld/src/CodeUp/Main.java
+++ HelloWorld/src/CodeUp/Main.java
@@ -10,12 +10,15 @@ public class Main {
 		
 		int n = sc.nextInt();
 		
-		for(int i = 0; i < n; i++) {
-			for(int j = 1; j < n - i; j++){
-				System.out.print(" "); 
+		for(int i = 1; i < n; i++) {
+			for(int a = 1; a <= i; a++){
+				System.out.print("*");
 			}
-			for(int j = 0; j < 2 * i + 1; j++){
-				System.out.print("*"); 
+			System.out.println();
+		}
+		for(int i = 1; i <= n; i++) {
+			for(int a = n; a >= i; a--){
+				System.out.print("*");
 			}
 			System.out.println();
 		}
